b09d97e33edf164876f06c42c3627032bbdc2bca
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-12-04 12:52:09 -0600

    update code so that JS returns second list item only

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,13 @@ $(document).ready(function () {
     event.preventDefault();
     $("#groceryInput").hide();
 
-    var groceryList = document.getElementsByClassName('item').value;
+    var grocery1 = document.getElementById('input1').value;
+    var grocery2 = document.getElementById('input2').value;
+    console.log(grocery1, grocery2);
+    var groceryList = (grocery1, grocery2);
     groceryItems.push(groceryList);
     console.log(groceryItems);
-    groceryItems.forEach(function (groceryItem) {
+    groceryItems.forEach(function(groceryItem) {
       str += '<li>' + groceryItem + '</li>';
     })
 
